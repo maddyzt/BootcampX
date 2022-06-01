1aef2eb9296dadaa0720f1639af51ee99b050031
Maddy Tan <maddytan7@gmail.com>
2022-06-01 18:37:27 +0000

    includes node app with student/teacher command line apps

--- 4_queries/name_of_teacher.sql
+++ 4_queries/name_of_teacher.sql
@@ -10,10 +10,10 @@
 -- This query needs to select data for a cohort with a specific name, use 
 -- 'JUL02' for the cohort's name here.
 
-SELECT DISTINCT teachers.name as teacher, cohorts.name
-FROM cohorts
-JOIN students ON cohort_id = cohorts.id
-JOIN assistance_requests ON students.id = student_id
-JOIN teachers ON teacher_id = teachers.id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
 ORDER BY teacher;
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+
 const cohort_name = process.argv[2];
 const max_result = process.argv[3];
 
 pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${cohort_name}%'
+ORDER BY teacher
 LIMIT ${max_result};
 `)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
-})
-;
-
+});
